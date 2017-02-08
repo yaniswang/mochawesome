59dcb4320df5ed295cf87f8a3e9ee7fdc1047b8f
Yanis Wang <yanis.wangy@alibaba-inc.com>
2017-02-08 09:31:44 +0800

    Support show hosts Support unicode file name

--- test/basic/test.js
+++ test/basic/test.js
@@ -1,38 +1,37 @@
 var should = require('should');
 
-describe('Master Test Suite', function () {
-  describe('Test Suite - Basic', function () {
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-    it('failing test', function (done) {
-      false.should.be.ok;
-      done();
-    });
+describe('sample/测试_spec : chrome', function () {
+  it('url: http://www.baidu.com', function(){
+      aaa
   });
+  
+  it('waitBody: ', function(){
+      bbb
+  });
+  
+  it('click: wd ( #kw, 183, 14, 0 )', function(done){
+      setTimeout(done, 200);
+  });
+  
+  it('sendKeys: mp3{ENTER}', function(){
+      
+  });
+});
 
-  describe('Test Suite - Failed After Each', function () {
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-    xit('pending test', function (done) {
-      done();
-    });
-    it('failing test', function (done) {
-      false.should.be.ok;
-      done();
-    });
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-    xit('pending test', function (done) {
-      done();
-    });
-    afterEach('failing afterEach hook', function () {
-      console.log('a');
-    });
+describe('sample/测试_spec : ie 11', function () {
+  it('url: http://www.baidu.com', function(done){
+      setTimeout(done, 800);
+  });
+  
+  it('waitBody: ', function(){
+      
+  });
+  
+  it('click: wd ( #kw, 183, 14, 0 )', function(){
+      
+  });
+  
+  it('sendKeys: mp3{ENTER}', function(){
+      
   });
 });
